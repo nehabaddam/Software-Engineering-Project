f465bbb3b66a783fc48125c39901befba35b132a
purandharamaharshi06 <98600455+purandharamaharshi06@users.noreply.github.com>
2022-04-22 14:18:32 -0500

    Add files via upload

---
+++ Code/Phase-1/user/includes/header.php
@@ -0,0 +1,17 @@
+<div class="navbar navbar-fixed-top" >
+ <header class="header" style="background-color: Black; outline:1px solid white;">
+
+              <div class="sidebar-toggle-box">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+              </div>
+            <!--logo start-->
+
+            <a href="dashboard.php" class="logo" style = "color:white; " ><p style="font-family:verdana;" ><b>The Bug Tracking System | Customer</p></b></a>
+                
+            <div class="top-menu">
+            	<ul class="nav pull-right top-menu">
+                    <li><a class="logout" href="logout.php" style = "color:black; background-color: white; outline:1px solid white; "><b>Logout</b></a></li>
+            	</ul>
+            </div>
+        </header>
+</div>
