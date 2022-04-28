6dafaab0bd4c5aa79c9829eab92904e2710fea9a
reshmi611 <98570466+reshmi611@users.noreply.github.com>
2022-04-27 22:22:01 -0500

    Add files via upload

---
+++ Code/Phase-2/admin/include/header.php
@@ -0,0 +1,35 @@
+<div class="navbar navbar-fixed-top" >
+		<div class="navbar-inner" style = "background-color: black">
+			<div class="container" >
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse" style="background-color: #a2e6eb;" >
+					<i class="icon-reorder shaded"></i>
+				</a>
+
+			  	<a class="brand" href="index.html" class="logo" style = "color: white">
+			  	The Bug Tracking System-(BTS) | Manager 
+			  	</a>
+				
+				<div class="nav-collapse collapse navbar-inverse-collapse" >
+					
+					<ul class="nav pull-right" >
+						<li><a href="#" style = "color: white"> 
+							Manager 
+						</a></li>
+						<li class="nav-user dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+								<img src="images/user.png" class="nav-avatar" />
+								<b class="caret"></b>
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="admin-profile.php">Profile</a></li>
+								<li><a href="change-password.php">Change Password</a></li>
+								<li class="divider"></li>
+								<li><a href="logout.php">Logout</a></li>
+							</ul>
+						</li>
+					</ul>
+					
+				</div><!-- /.nav-collapse -->
+			</div>
+		</div><!-- /navbar-inner -->
+	</div><!-- /navbar -->
