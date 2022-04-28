cae55e7790139a8e2066e6a34f37441f788426a6
reshmi611 <98570466+reshmi611@users.noreply.github.com>
2022-04-27 22:22:37 -0500

    Add files via upload

---
+++ Code/Phase-3/admin/include/footer.php
@@ -0,0 +1,10 @@
+	<div class="footer" >
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; <?php  $year=date("Y");
+echo htmlentities($year);
+
+			?> BTS </b> All rights reserved.
+		</div>
+	</div>
