a8bd7a1a565b255c86639eb815656b0fc526cd39
nehabaddam <98571354+nehabaddam@users.noreply.github.com>
2022-04-28 00:18:16 -0500

    Delete Code/Phase-1/fonts/users directory

--- Code/Phase-1/fonts/users/includes/footer.php
+++
@@ -1,8 +0,0 @@
- <footer class="site-footer" style = "color:black ;background-color:grey;">
-          <div class="text-center">
-              <?php echo date('Y')?> - The Bug Tracking System
-              <a href="#" class="go-top">
-                  <i class="fa fa-angle-up"></i>
-              </a>
-          </div>
-      </footer>
